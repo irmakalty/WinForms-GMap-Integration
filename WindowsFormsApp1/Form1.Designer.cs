6ff2b54d3c198d1d08c69671e4dae916820083bb
irmak <irmak@LAPTOP-A176MBH2>
2026-04-17 20:43:28 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEnlem = new System.Windows.Forms.TextBox();
+            this.txtBoylam = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // txtEnlem
+            // 
+            this.txtEnlem.Location = new System.Drawing.Point(110, 35);
+            this.txtEnlem.Name = "txtEnlem";
+            this.txtEnlem.Size = new System.Drawing.Size(177, 22);
+            this.txtEnlem.TabIndex = 0;
+            // 
+            // txtBoylam
+            // 
+            this.txtBoylam.Location = new System.Drawing.Point(110, 77);
+            this.txtBoylam.Name = "txtBoylam";
+            this.txtBoylam.Size = new System.Drawing.Size(177, 22);
+            this.txtBoylam.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(25, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enlem:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(25, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Boylam:";
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnGoster.Location = new System.Drawing.Point(173, 163);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(114, 79);
+            this.btnGoster.TabIndex = 4;
+            this.btnGoster.Text = "Haritada Göster";
+            this.btnGoster.UseVisualStyleBackColor = false;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(343, 35);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(334, 371);
+            this.gMapControl1.TabIndex = 5;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(722, 496);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoylam);
+            this.Controls.Add(this.txtEnlem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEnlem;
+        private System.Windows.Forms.TextBox txtBoylam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGoster;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 1. Form açıldığında 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Harita sağlayıcısını Google Maps olarak seçiyoruz
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+
+            // Başlangıç noktası koordinatları (Örnek: İstanbul)
+            gMapControl1.Position = new PointLatLng(41.0082, 28.9784);
+
+            // Yakınlaştırma (Zoom) ayarları
+            gMapControl1.MinZoom = 2;
+            gMapControl1.MaxZoom = 18;
+            gMapControl1.Zoom = 10;
+
+            // Haritayı fare ile kaydırmak için sol tuşu atıyoruz
+            gMapControl1.DragButton = MouseButtons.Left;
+        }
+
+        // 2. "Haritada Göster" butonuna tıklandığında çalışacak kodlar
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                double enlem = Convert.ToDouble(txtEnlem.Text.Replace(".", ","));
+                double boylam = Convert.ToDouble(txtBoylam.Text.Replace(".", ","));
+
+              
+                gMapControl1.Position = new PointLatLng(enlem, boylam);
+                gMapControl1.Zoom = 14;
+
+                // Varsa eski işaretçileri temizle
+                gMapControl1.Overlays.Clear();
+
+                
+                GMapOverlay isaretciKatmani = new GMapOverlay("Isaretciler");
+
+                // Kırmızı renkli Google işaretçisini (Marker) oluşturma
+                GMarkerGoogle isaretci = new GMarkerGoogle(new PointLatLng(enlem, boylam), GMarkerGoogleType.red_dot);
+
+                // İşaretçiyi haritaya ekle
+                isaretciKatmani.Markers.Add(isaretci);
+                gMapControl1.Overlays.Add(isaretciKatmani);
+            }
+            catch (Exception)
+            {
+                // Hatalı veya boş veri girilirse programın çökmesin diye
+                MessageBox.Show("Lütfen geçerli bir koordinat giriniz!\nÖrnek: 41,0082", "Hatalı Giriş", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
